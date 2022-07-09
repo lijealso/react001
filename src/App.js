09d39d17d7f2350b133b664e0031890985cff3d7
lijealso <lijealso@gmail.com>
2022-07-09 17:46:51 +0100

    add title and author

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <>
       <h1>Comentários</h1>
-      <Comment />
-      <Comment />
-      <Comment />
+      <Comment title="Comentário Um" author="Manuel Silva"/>
+      <Comment title="Comentário Dois" author="Sara Gomes" />
+      <Comment title="Comentário Três" author="Rui Maria"/>
     </>
   );
 }
